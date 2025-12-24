d800721595c3988f0cee12a6c5922b2f420dd8b4
PYmul <pymul.tech@gmail.com>
2025-12-23 23:47:58 -0300

    Atualizar o arcs.txt

--- PYmul/cli.py
+++ PYmul/cli.py
@@ -1,12 +1,26 @@
-import sys
-from pymul.core.dispatcher import dispatch
-
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: pymul <command> <target>")
-        sys.exit(1)
-
-    dispatch(sys.argv[1:])
-
-if __name__ == "__main__":
-    main()
+pymul/
+│
+├── pymul/
+│   ├── __init__.py
+│   ├── cli.py              # Entrada principal
+│   ├── core/
+│   │   ├── dispatcher.py   # Decide o que fazer
+│   │   ├── os_detect.py    # Detecta SO
+│   │   └── executor.py     # Executa comandos
+│   │
+│   ├── managers/
+│   │   ├── base.py         # Classe abstrata
+│   │   ├── pip_manager.py
+│   │   ├── apt_manager.py
+│   │   ├── brew_manager.py
+│   │   └── choco_manager.py
+│   │
+│   └── commands/
+│       ├── install.py
+│       ├── env.py
+│       ├── run.py
+│       └── sysinfo.py
+│
+├── pyproject.toml
+├── README.md
+└── LICENSE
