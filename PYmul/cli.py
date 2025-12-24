bbd321766a5847279bf82f7c558b48e3f62f37eb
PYmul <pymul.tech@gmail.com>
2025-12-23 23:46:12 -0300

    Atualizar o cli.py

--- PYmul/cli.py
+++ PYmul/cli.py
@@ -1,1 +1,12 @@
-.
+import sys
+from pymul.core.dispatcher import dispatch
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: pymul <command> <target>")
+        sys.exit(1)
+
+    dispatch(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
